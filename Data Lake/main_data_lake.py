f0032b60ea88fd6d00e79c9e7bd05538c61a17e5
Tom Careghi <tom.careghi@gmail.com>
2022-03-13 18:15:30 +0100

    v1 scraping pinterest

--- Data Lake/main_data_lake.py
+++ Data Lake/main_data_lake.py
@@ -1,1 +1,4 @@
-print("ok")
+from scrape_pinterest import *
+
+url = "https://www.pinterest.fr/search/pins/?q=Tour%20Eiffel&rs=typed&term_meta[]=Tour%7Ctyped&term_meta[]=Eiffel%7Ctyped"
+web_scraping_Pinterest(url)
